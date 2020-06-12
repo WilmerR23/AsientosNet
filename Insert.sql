606de7b3634c1dd5fe36a2c84d1858d37907238f
kwilram23 <kwilram23@gmail.com>
2020-06-12 19:39:06 -0400

    segundo commit

---
+++ Insert.sql
@@ -0,0 +1,8 @@
+
+
+  INSERT INTO Asientoes VALUES 
+  (1,'Primer asiento',GETDATE(),12312323,234323,'Credito'),
+  (2,'Segundo asiento',GETDATE(),42423432,2342343,'Credito'),
+  (3,'Tercer asiento',GETDATE(),33343444,5356574,'Credito'),
+  (4,'Cuarto asiento',GETDATE(),23423424,3213131,'Credito'),
+  (5,'Quinto asiento',GETDATE(),23423432,45353453,'Credito')
